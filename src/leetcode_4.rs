39c8fe04b79ff985c2bc0d06f1b763297ec2521f
Lucas Bombarda <lucas@devdock.com.br>
2024-02-14 17:47:12 -0300

    added leetcode_4

---
+++ src/leetcode_4.rs
@@ -0,0 +1,39 @@
+// LeetCode:
+// https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+use std::cmp::max;
+use std::collections::HashMap;
+
+struct Solution;
+
+impl Solution {
+    pub fn length_of_longest_substring(s: String) -> i32 {
+        let mut result = 0;
+        let mut start = 0;
+        let mut end = 0;
+        let mut map = HashMap::new();
+
+        for (idx, c) in s.chars().enumerate() {
+            if map.contains_key(&c) {
+                start = max(*map.get(&c).unwrap() + 1, start);
+            }
+            map.insert(c, idx);
+            end = idx + 1;
+            result = max(result, end - start);
+        }
+        result as i32
+
+    }
+}
+
+fn main() {
+    let s1 = "abcabcbb".into();
+    let s2 = "bbbbb".into();
+    let s3 = "pwwkew".into();
+    let s4 = "dvdf".into();
+
+    println!("s1 - {}", Solution::length_of_longest_substring(s1));
+    println!("s2 - {}", Solution::length_of_longest_substring(s2));
+    println!("s3 - {}", Solution::length_of_longest_substring(s3));
+    println!("s4 - {}", Solution::length_of_longest_substring(s4));
+}
