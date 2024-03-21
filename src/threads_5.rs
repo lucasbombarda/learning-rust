10f444d6313517af3cd12416dfd0019169728f0d
Lucas Bombarda <lucas@devdock.com.br>
2024-03-21 19:49:26 -0300

    changed logic on threads_5

--- src/threads_5.rs
+++ src/threads_5.rs
@@ -6,19 +6,20 @@ use util::random::generate_random_number;
 
 fn main() {
     let (tx, rx) = mpsc::channel();
+    let half_sec = std::time::Duration::from_millis(500);
 
-    for _ in 0..10 {
-        let tx = tx.clone();
-        thread::spawn(move || {
-            let number = generate_random_number(1, 10);
+    thread::spawn(move || {
+        for _ in 0..10 {
+            let tx = tx.clone();
+            let number = generate_random_number(1, 100);
             tx.send(number).unwrap();
-        });
-    }
+            println!("Sent: {}", number);
+            thread::sleep(half_sec);
+        }
+    });
 
-    let mut numbers = Vec::new();
     for _ in 0..10 {
         let n = rx.recv().unwrap();
-        numbers.push(n * n);
+        println!("Received and power of two: {} - {}", n, n * n);
     }
-    println!("{:?}", numbers);
 }
