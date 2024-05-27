d7fe4849f90ed4997d88cf3ecd6c46df9cc60f1d
Lucas Bombarda <lucas@devdock.com.br>
2024-05-27 10:52:41 -0300

    fix: file structures

---
+++ src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    println!("This is a repo for my studies in Rust.");
+    println!("To run the code, pick a binary file and run it with cargo run --bin <binary_file>");
+    println!("To run the code in sorts.rs, run cargo run --bin sorts");
+}
